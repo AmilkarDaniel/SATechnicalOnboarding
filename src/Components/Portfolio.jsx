e888de574e8bba4aeaedba992c2b579e676907da
Amilkar Daniel <danielquezo296@gmail.com>
2024-04-06 20:15:22 -0400

    portfolio data update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Nasa Space Apps Challenge 2022",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Pitch: Project StarBlue. StarBlue is a didactic learning software.",
+    url: "https://www.youtube.com/watch?v=bD3fhYZ5h5A",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Landing Page Design of Velada del Año IV",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Copy of the design of La Velada del Año IV website using Astro and Tailwind technologies.",
+    url: "https://github.com/AmilkarDaniel/la-velada-web-oficial",
   },
   {
-    title: "My Resume Site",
+    title: "Naabol Website Design",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Design of naabol's website using the figma tool.",
+    url: "https://www.figma.com/file/X76f6OuShTgzRJ5CtDqq5I/Design-Naabol?type=design&node-id=0%3A1&mode=design&t=Zgzn8UuZNHz0X90q-1",
   },
 ];
 
